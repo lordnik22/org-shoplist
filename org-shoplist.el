583bd9955b7c475f28e1f3cb979d3d95049307d5
lordnik <michi.b_98@hotmail.com>
2018-03-11 20:53:18 +0100

    Basic implementation of an ingredient

--- org-shoplist.el
+++ org-shoplist.el
@@ -15,6 +15,61 @@
 ;;; Commentary:
 ;; There is nothing done, yet.
 ;;; Code:
-;; Will follow...
+(defcustom org-shoplist-ingredient-units '()
+  "Units for specifing the amount of a ingredient."
+  :type '(repeat string)
+  :group 'org-shoplist)
+
+(defconst org-shoplist-ingredient-regex
+  (concat "(\\([1-9][0-9]*\\)"
+	  "\\("
+	  (mapconcat 'identity
+		     org-shoplist-ingredient-units
+		     "\\|")
+	  "\\)?"
+	  " \\(.+?\\))")
+  "Regex for finding ingredients in strings.")
+
+(defun org-shoplist-str-to-ing (str)
+  "Convert 'STR' to a ing struct.
+When 'STR' is empty or can't be converted returns nil.
+Empty ings are nil or nothing."
+  (if (eq nil (string-match org-shoplist-ingredient-regex str))
+      '()
+    (org-shoplist--str-to-ing-extract-match '() str)))
+
+(defun org-shoplist--str-to-ing-extract-match (ing-list str)
+  "Extracts every ingredient in (match-string).
+'ING-LIST' contains the found ingredients.
+'START-INDEX' defines the starting index where ing starts
+'STR' defines the string which contains the ingredients.
+A ingredient is a list of three elements."
+  (setq ing-list
+	(cons (list (string-to-number (match-string 1 str))
+		    (if (string= (match-string 2 str) "")
+			nil
+		      (match-string 2 str))
+		    (match-string 3 str))
+	      ing-list))
+  (if (eq nil (string-match org-shoplist-ingredient-regex str (match-end 0)))
+      (reverse ing-list)
+    (org-shoplist--str-to-ing-extract-match ing-list str)))
+
+(defun org-shoplist-ing-get-amount (ing)
+  "Get amount of ingredient as a number.
+'ING' is a list representing a ingredient."
+  (car ing))
+
+(defun org-shoplist-ing-get-unit (ing)
+  "Get unit of ingredient as a string.
+'ING' is a list representing a ingredient."
+  (car (cdr ing)))
+
+(defun org-shoplist-ing-get-name (ing)
+  "Get name of ingredient as a string.
+'ING' is a list representing a ingredient.
+Beaware that the name also includes adjectives.
+Exmaple: '(100 'g' 'crushed Nuts) => 'crushed Nuts'"
+  (car (cdr (cdr ing))))
 (provide 'org-shoplist)
 ;;; org-shoplist.el ends here
--- test/org-shoplist-test.el
+++ test/org-shoplist-test.el
@@ -1,8 +1,64 @@
 ;;; org-shoplist-test.el --- Tests for org-shoplist
 ;;; Commentary:
+;; Tests the data structures and functions of org-shoplist
 ;;; Code:
+(setq org-shoplist-ingredient-units '("g" "ml"))
+(setq org-shoplist-ingredient-regex
+  (concat "(\\([1-9][0-9]*\\)"
+	  "\\("
+	  (mapconcat 'identity
+		     org-shoplist-ingredient-units
+		     "\\|")
+	  "\\)?"
+	  " \\(.+?\\))")) 
+
 (ert-deftest org-shoplist-test/feeling-better? ()
     "Checks if it's a good day to program."
   (should (= 1 1)))
+
+(ert-deftest org-shoplist-test/str-to-ing-empty-str ()
+  (should (equal nil (org-shoplist-str-to-ing ""))))
+
+(ert-deftest org-shoplist-test/str-to-ing-no-ing-in-str ()
+  (should (equal nil (org-shoplist-str-to-ing "foo"))))
+
+(ert-deftest org-shoplist-test/str-to-ing-empty-ing-in-str ()
+  (should (equal nil (org-shoplist-str-to-ing "()"))))
+
+(ert-deftest org-shoplist-test/str-to-ing-one-ing-in-str ()
+  (should (equal '((100 nil "Nuts")) (org-shoplist-str-to-ing "(100 Nuts)"))))
+
+(ert-deftest org-shoplist-test/str-to-ing-ing-surrounded-with-verb-and-adj ()
+  (should (equal '((100 nil "Nuts")) (org-shoplist-str-to-ing "crush (100 Nuts) massively"))))
+
+(ert-deftest org-shoplist-test/str-to-ing-with-measure ()
+  (should (equal '((100 "g" "Nuts")) (org-shoplist-str-to-ing "(100g Nuts)"))))
+
+(ert-deftest org-shoplist-test/str-to-ing-with-adj-in-ing ()
+  (should (equal '((100 "g" "crushed Nuts")) (org-shoplist-str-to-ing "(100g crushed Nuts)"))))
+
+(ert-deftest org-shoplist-test/str-to-ing-empty-ing+ing-in-one-string ()
+  "One mepty ing and a normal ing in one string."
+  (should (equal '((100 "g" "Nuts")) (org-shoplist-str-to-ing "()(100g Nuts)"))))
+
+(ert-deftest org-shoplist-test/str-to-ing-two-ings-in-one-string ()
+  (should (equal '((100 "ml" "Milk")
+		   (100 "g" "Nuts")) (org-shoplist-str-to-ing "(100ml Milk)(100g Nuts)"))))
+
+(ert-deftest org-shoplist-test/str-to-ing-three-ings-in-one-string ()
+  (should (equal '((100 "ml" "Milk")
+		   (100 "g" "Nuts")
+		   (1 nil "Lemon")) (org-shoplist-str-to-ing "(100ml Milk)(100g Nuts)(1 Lemon)"))))
+
+(ert-deftest org-shoplist-test/str-to-ing-two-ings-in-sentence ()
+  (should (equal '((100 "ml" "Milk")
+		   (100 "g" "Nuts")) (org-shoplist-str-to-ing "mix (100ml Milk) and (100g Nuts) toghter"))))
+
+(ert-deftest org-shoplist-test/str-to-ing-nested-parenthese-one-ing ()
+  (should (equal '((100 "ml" "Milk")) (org-shoplist-str-to-ing "((100ml Milk))"))))
+
+(ert-deftest org-shoplist-test/str-to-ing-nested-parenthese-two-ing ()
+  (should (equal '((100 "ml" "Milk")
+		   (100 "g" "Nuts")) (org-shoplist-str-to-ing "((100ml Milk)(100g Nuts))"))))
 
 ;;; org-shoplist-test.el ends here
