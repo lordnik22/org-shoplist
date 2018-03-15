5402be9bedca613782c24dc8b67fbb31218b3d0a
lordnik <michi.b_98@hotmail.com>
2018-03-15 21:18:20 +0100

    Remove bad variable in test-helper

--- test/test-helper.el
+++ test/test-helper.el
@@ -2,6 +2,4 @@
 ;;; Commentary:
 ;; Helps out org-shoplist tests
 ;;; Code:
-(defvar org-shoplist-default-ingredient (org-shoplist-ing-create "100g" "Nuts")
-  "Default ingredient that is used for many tests.")
 ;;; test-helper.el ends here
