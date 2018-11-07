dc786943611679d82b4927209fc11edff2f47c57
lordnik22 <michi.b_98@hotmail.com>
2018-11-07 23:57:18 +0100

    Correct dots in test assertions

--- test/org-shoplist-recipe-test.el
+++ test/org-shoplist-recipe-test.el
@@ -8,12 +8,12 @@
 
 (ert-deftest org-shoplist-test/recipe-create-nil ()
   "Should error when passing no name for recipe."
-  (should (equal '(error "Invalid name for recipe: ''")
+  (should (equal '(error "Invalid name for recipe: ’’")
 		 (should-error (org-shoplist-recipe-create nil)))))
 
 (ert-deftest org-shoplist-test/recipe-create-empty-string-name ()
   "Should error when passing no name for recipe."
-  (should (equal '(error "Invalid name for recipe: ''")
+  (should (equal '(error "Invalid name for recipe: ’’")
 		 (should-error (org-shoplist-recipe-create "")))))
 
 
@@ -82,7 +82,7 @@
    (lambda ()
      (insert "* ")
      (goto-char (point-min))
-     (should (equal '(error "Invalid name for recipe: ''")
+     (should (equal '(error "Invalid name for recipe: ’’")
 		    (should-error (org-shoplist-recipe-read))))
      (should (= (point) (point-min))))))
 
